cf22eb99b7d4c0a857833cf6493e628da44e749c
nag2548 <nadine.grabmair@gmx.de>
2025-12-02 19:10:08 +0100

    day2, part 2

---
+++ src/day2_part2.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+
+def get_input() -> str:
+    input_path = Path(__file__).parent.parent / "inputs" / "day2.txt"
+    with open(input_path, "r") as f:
+        return f.read()
+
+
+def get_invalid_ids_sum():
+    line = get_input()
+    id_list = [pair.split("-") for pair in line.split(",")]
+    id_sum = 0
+
+    for pair in id_list:
+        for i in range(int(pair[0]), int(pair[1]) + 1):
+            string_value = str(i)
+            for chunk in range(1, (len(string_value) // 2) + 1):
+                if len(string_value) % chunk != 0:
+                    continue
+                chunks = [
+                    string_value[j : j + chunk]
+                    for j in range(0, len(string_value), chunk)
+                ]
+                if all(item == chunks[0] for item in chunks):
+                    id_sum += i
+                    break
+
+    print(f"invalid ids sum: {id_sum}")
+    return id_sum
+
+
+if __name__ == "__main__":
+    get_invalid_ids_sum()
--- src/tests/day2_tests.py
+++ src/tests/day2_tests.py
@@ -1,9 +1,18 @@
 import unittest
 
-from src import day2
+import src.day2 as part1
+import src.day2_part2 as part2
 
 
 class Day2Tests(unittest.TestCase):
     def test_part_1(self):
-        sum = day2.get_invalid_ids_sum()
-        self.assertEqual(12850231731, sum)
+        id_sum = part1.get_invalid_ids_sum()
+        self.assertEqual(12850231731, id_sum)
+
+    def test_part_2(self):
+        id_sum = part2.get_invalid_ids_sum()
+        self.assertEqual(24774350322, id_sum)
+
+
+if __name__ == "__main__":
+    unittest.main()
