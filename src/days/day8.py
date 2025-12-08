d99dc829cc014a71e789ddd827a521ba099f81e4
nag2548 <nadine.grabmair@gmx.de>
2025-12-08 23:35:05 +0100

    day 8 refactor

--- src/days/day8.py
+++ src/days/day8.py
@@ -1,61 +1,31 @@
 from collections import defaultdict
 from operator import itemgetter
+from typing import Any
 
 import numpy as np
 
 from src.helpers.file_helper import read_file_as_list
 
 
-def find(x, graph):
-    if graph[x] != x:
-        graph[x] = find(graph[x], graph)
-    return graph[x]
-
-
-def union(a, b, graph):
-    root_a = find(a, graph)
-    root_b = find(b, graph)
-    if root_a != root_b:
-        graph[root_b] = root_a
-
-
 def solve_1(connections):
     lines = read_file_as_list("day8.txt")
     vectors = [tuple(map(int, line.split(","))) for line in lines]
 
-    distances = []
-    for i, a in enumerate(vectors):
-        x1, y1, z1 = a
-        for b in vectors[i + 1 :]:
-            x2, y2, z2 = b
-            distances.append(
-                (
-                    a,
-                    b,
-                    np.sqrt(
-                        np.pow(x1 - x2, 2) + np.pow(y1 - y2, 2) + np.pow(z1 - z2, 2)
-                    ),
-                )
-            )
-
+    distances, nodes = init_edges_and_nodes(vectors)
     edges = sorted(distances, key=itemgetter(2))[slice(connections)]
-
-    graph = {}
+    graph = defaultdict(list)
     for a, b, _ in edges:
-        graph.setdefault(a, a)
-        graph.setdefault(b, b)
+        graph[a].append(b)
+        graph[b].append(a)
 
-    for a, b, _ in edges:
-        union(a, b, graph)
-
-    components = {}
+    groups = []
     for node in graph:
-        root = find(node, graph)
-        components.setdefault(root, []).append(node)
+        group = []
+        if node in nodes:
+            dfs(node, nodes, graph, group)
+        groups.append(group)
 
-    lengths = [
-        len(group) for group in sorted(components.values(), key=len, reverse=True)
-    ]
+    lengths = [len(group) for group in sorted(groups, key=len, reverse=True)]
     result = np.prod(lengths[:3])
     print(f"result: {result}")
     return result
@@ -65,6 +35,32 @@ def solve_2():
     lines = read_file_as_list("day8.txt")
     vectors = [tuple(map(int, line.split(","))) for line in lines]
 
+    distances, nodes = init_edges_and_nodes(vectors)
+
+    edges = sorted(distances, key=itemgetter(2))
+    graph = defaultdict(list)
+    node_copy = set(nodes)
+    last_edge = None
+    for a, b, _ in edges:
+        graph[a].append(b)
+        graph[b].append(a)
+        last_edge = (a, b)
+
+        for node in graph:
+            if node in node_copy:
+                dfs(node, node_copy, graph, [])
+
+        if not node_copy:
+            break
+        node_copy = set(nodes)
+
+    a, b = last_edge
+    result = a[0] * b[0]
+    print(f"result: {result}")
+    return result
+
+
+def init_edges_and_nodes(vectors: list[tuple[int, ...]]) -> tuple[list[Any], set[Any]]:
     distances = []
     nodes = set()
     for i, a in enumerate(vectors):
@@ -81,35 +77,15 @@ def solve_2():
                     ),
                 )
             )
-
-    edges = sorted(distances, key=itemgetter(2))
-    graph = defaultdict(list)
-    node_copy = set(nodes)
-    last_edge = None
-    for a, b, _ in edges:
-        graph[a].append(b)
-        graph[b].append(a)
-        last_edge = (a, b)
-
-        for node in graph:
-            if node in node_copy:
-                dfs(node, node_copy, graph)
-
-        if not node_copy:
-            break
-        node_copy = set(nodes)
-
-    a, b = last_edge
-    result = a[0] * b[0]
-    print(f"result: {result}")
-    return result
+    return distances, nodes
 
 
-def dfs(node, node_copy, graph):
+def dfs(node, node_copy, graph, group):
     node_copy.remove(node)
+    group.append(node)
     for child in graph[node]:
         if child in node_copy:
-            dfs(child, node_copy, graph)
+            dfs(child, node_copy, graph, group)
 
 
 if __name__ == "__main__":
