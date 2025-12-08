b31d24b4e629e8ecf9419cd0a32630e27f63c2f7
nag2548 <nadine.grabmair@gmx.de>
2025-12-08 19:33:25 +0100

    day 8, part 1

--- src/days/day8.py
+++ src/days/day8.py
@@ -5,8 +5,21 @@ import numpy as np
 from src.helpers.file_helper import read_file_as_list
 
 
-def solve_1():
-    lines = read_file_as_list("day8_example.txt")
+def find(x, graph):
+    if graph[x] != x:
+        graph[x] = find(graph[x], graph)
+    return graph[x]
+
+
+def union(a, b, graph):
+    root_a = find(a, graph)
+    root_b = find(b, graph)
+    if root_a != root_b:
+        graph[root_b] = root_a
+
+
+def solve_1(connections):
+    lines = read_file_as_list("day8.txt")
     vectors = [tuple(map(int, line.split(","))) for line in lines]
 
     distances = []
@@ -23,11 +36,29 @@ def solve_1():
                     ),
                 )
             )
-    for d in sorted(distances, key=itemgetter(2)):
-        print(d)
 
-    return 0
+    edges = sorted(distances, key=itemgetter(2))[slice(connections)]
+
+    graph = {}
+    for a, b, _ in edges:
+        graph.setdefault(a, a)
+        graph.setdefault(b, b)
+
+    for a, b, _ in edges:
+        union(a, b, graph)
+
+    components = {}
+    for node in graph:
+        root = find(node, graph)
+        components.setdefault(root, []).append(node)
+
+    lengths = [
+        len(group) for group in sorted(components.values(), key=len, reverse=True)
+    ]
+    result = np.prod(lengths[:3])
+    print(f"result: {result}")
+    return result
 
 
 if __name__ == "__main__":
-    solve_1()
+    solve_1(10)
--- src/tests/test_day8.py
+++ src/tests/test_day8.py
@@ -5,8 +5,8 @@ from src.days import day8
 
 class TestDay8(unittest.TestCase):
     def test_part_1(self):
-        result = day8.solve_1()
-        self.assertEqual(40, result)
+        result = day8.solve_1(1000)
+        self.assertEqual(96672, result)
 
 
 if __name__ == "__main__":
