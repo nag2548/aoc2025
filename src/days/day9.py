eb86337e16f3c4a57e8493f5bb5c20232808fd1a
Nadine Grabmair <nadine.grabmair@gmx.de>
2025-12-09 22:20:19 +0100

    day 9, part 2

--- src/days/day9.py
+++ src/days/day9.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 from operator import itemgetter
 
 from shapely import box, Polygon
@@ -25,15 +26,12 @@ def solve_2():
     tiles = [tuple(map(int, line.split(","))) for line in lines]
     pairs = [
         (a, b, (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1))
-        for i, a in enumerate(tiles)
-        for b in tiles[i + 1 :]
+        for a, b in combinations(tiles, 2)
     ]
-    pairs.sort(key=itemgetter(2), reverse=True)
 
     largest_area = None
     poly = Polygon(tiles)
-    for pair in pairs:
-        (ax, ay), (bx, by), area = pair
+    for (ax, ay), (bx, by), area in sorted(pairs, key=itemgetter(2), reverse=True):
         if poly.contains(box(min(ax, bx), min(ay, by), max(ax, bx), max(ay, by))):
             largest_area = area
             break
