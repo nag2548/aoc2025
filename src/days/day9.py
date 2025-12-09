230b80006b09f368930e351f8a5b2f714f014a0f
Nadine Grabmair <nadine.grabmair@gmx.de>
2025-12-09 22:09:48 +0100

    day 9, part 2

--- src/days/day9.py
+++ src/days/day9.py
@@ -1,5 +1,7 @@
 from operator import itemgetter
 
+from shapely import box, Polygon
+
 from src.helpers.file_helper import read_file_as_list
 
 
@@ -18,5 +20,28 @@ def solve():
     return result
 
 
+def solve_2():
+    lines = read_file_as_list("day9.txt")
+    tiles = [tuple(map(int, line.split(","))) for line in lines]
+    pairs = [
+        (a, b, (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1))
+        for i, a in enumerate(tiles)
+        for b in tiles[i + 1 :]
+    ]
+    pairs.sort(key=itemgetter(2), reverse=True)
+
+    largest_area = None
+    poly = Polygon(tiles)
+    for pair in pairs:
+        (ax, ay), (bx, by), area = pair
+        if poly.contains(box(min(ax, bx), min(ay, by), max(ax, bx), max(ay, by))):
+            largest_area = area
+            break
+
+    print(f"result day 9, part 2: {largest_area}")
+    return largest_area
+
+
 if __name__ == "__main__":
     solve()
+    solve_2()
--- src/tests/test_day9.py
+++ src/tests/test_day9.py
@@ -6,7 +6,11 @@ from src.days import day9
 class TestDay9(unittest.TestCase):
     def test_part1(self):
         result = day9.solve()
-        self.assertEqual(50, result)
+        self.assertEqual(4776100539, result)
+
+    def test_part2(self):
+        result = day9.solve_2()
+        self.assertEqual(1476550548, result)
 
 
 if __name__ == "__main__":
